98afdbbb569240ecf726ee683f535ff775d77945
Rahul R Kaladgi <127622361+RAHULKALADGI@users.noreply.github.com>
2023-07-02 13:17:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,33 @@
 
-import React from "react";
+import React ,{useState} from "react";
 import './../styles/App.css';
 
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+const App = ()=> {
+    let [name , setName] = useState("");
+    let [pass , setPass] = useState("");
+    let [isLoggedIn , setIsLoggedIn] = useState(false);
+    function func() {
+        if(name && pass) {
+            setIsLoggedIn(true);
+        }
+    }
+    return (
+        <div>
+        <h1>Parent Component</h1>
+        {!isLoggedIn ? (
+        <div> 
+            <label for="name">Username:</label>
+            <input type="text" onChange={(event)=>setName(event.target.value)}/>
+            <label for="password">Password:</label>
+            <input type="password" onChange={(event)=>setPass(event.target.value)}/>
+            <button onClick={func}>Login</button>
+        </div>
+        ) : (
+            <p>You are logged in!</p>
+        )
+        }
+        </div>
+    )
 }
 
 export default App
